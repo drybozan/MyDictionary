ab58b2721838dd2d7b47b1a69c6b867e27b7b84b
Derya Bozan <86890722+drybozan@users.noreply.github.com>
2021-08-07 18:57:24 +0300

    Add files via upload

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+
+        TKey[] key;
+        TValue[] value;
+
+        public MyDictionary()
+        {
+            key = new TKey[0];
+            value = new TValue[0];
+
+        }
+        public void Add(TKey item1, TValue item2)
+        {
+
+            TKey[] temparraykey = key;
+            TValue[] temparrayvalues = value;
+
+            key = new TKey[key.Length + 1];
+            value = new TValue[value.Length + 1];
+            for (int i = 0; i < temparraykey.Length; i++)
+            {
+                key[i] = temparraykey[i];
+                value[i] = temparrayvalues[i];
+            }
+            key[key.Length - 1] = item1;
+            value[value.Length - 1] = item2;
+
+            Console.WriteLine(item1 + " numaralı  " + item2 + " adlı kullanıcı sistemde aktifleştirildi");
+        }
+
+          public int Count //dizi uzunluğunu görürüm
+        {
+            get { return key.Length; }
+
+            
+        }
+        public TKey[] Keys//key leri görürüm
+        {
+            get { return key; }
+            
+        }
+        public TValue[] Values//valuesleri görürüm
+        {
+            get { return value; }
+
+        }
+
+    }
+}
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> personel= new MyDictionary<int, string> ();
+            personel.Add(1,"derya");
+            personel.Add(2, "mevsim");
+            personel.Add(3, "ali");
+
+            Console.WriteLine("*********************************");
+            Console.WriteLine("Kayıtlı personel sayısı: " + personel.Count);
+
+            Console.WriteLine("********************************");
+
+            foreach (var key in personel.Keys)
+            {
+                Console.WriteLine("Personel No: " + key);
+            }
+
+            Console.WriteLine("******************************");
+
+            foreach (var value in personel.Values)
+            {
+                Console.WriteLine("Personel Adı: " + value);
+            }
+        }
+    }
+}
